b6595f4bc34f206e4ea9e6c70a36d46bb7fdefcb
yannickoo <drupal@yannickoo.de>
2014-05-11 16:45:53 +0200

    Revert "Fixes #1"
    This reverts commit 176914fac3f4878ea8c869a5c17a88bbe5dfee67.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   var input = document.querySelector('#input');
   var keycode = document.querySelector('#keycode');
 
-  input.addEventListener('keydown', function(e) {
+  input.addEventListener('keyup', function(e) {
     keycode.value = e.keyCode;
+  });
 
+  input.addEventListener('keydown', function(e) {
     if (input.value.length) {
       input.value = '';
     }
