176914fac3f4878ea8c869a5c17a88bbe5dfee67
yannickoo <drupal@yannickoo.de>
2014-05-11 16:30:25 +0200

    Fixes #1

--- js/app.js
+++ js/app.js
@@ -2,11 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var input = document.querySelector('#input');
   var keycode = document.querySelector('#keycode');
 
-  input.addEventListener('keyup', function(e) {
-    keycode.value = e.keyCode;
-  });
-
   input.addEventListener('keydown', function(e) {
+    keycode.value = e.keyCode;
+
     if (input.value.length) {
       input.value = '';
     }
